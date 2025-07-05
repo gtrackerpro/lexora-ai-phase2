05d8356b937f325806e5173359d0fc1ccca7b60d
warpagenticai <warpagenticai@gmail.com>
2025-07-05 23:00:50 +0530

    Fix JWT_SECRET environment variable loading

--- server/server.ts
+++ server/server.ts
@@ -1,3 +1,8 @@
+import dotenv from 'dotenv';
+
+// Load environment variables from .env file
+dotenv.config();
+
 import express from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
